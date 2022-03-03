6ec1a513ccd143c9a4037e020dd1d6965c5157c8
Vinit Hemadri <vineet.vh@gmail.com>
2022-03-03 11:25:35 +0530

    Connected with api and displayed advice

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,28 @@
+import { useState, useEffect } from "react";
+
 function Card() {
+  const [advice, setAdvice] = useState("");
+
+  const fetchAdvice = async () => {
+    try {
+      const response = await fetch("https://api.adviceslip.com/advice");
+      const data = await response.json();
+      // console.log(data.slip);
+      setAdvice(data.slip);
+    } catch (error) {
+      throw new Error("Something went wrong in fetching data!");
+    }
+  };
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
   return (
     <div className="container">
       <div className="card">
-        <p className="advice__id">Advice #114</p>
-        <p className="advice">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, vero
-          perferendis soluta iste accusamus facilis ipsa perspiciatis recusandae
-          error odit! Dolores quaerat modi obcaecati ratione mollitia
-        </p>
+        <p className="advice__id">Advice {`#${advice.id}`}</p>
+        <p className="advice">"{advice.advice}"</p>
         <img src={"assets/pattern-divider-desktop.svg"} alt="Divider" />
         <div className="dice">
           <img src={"assets/icon-dice.svg"} alt="Icon" />
